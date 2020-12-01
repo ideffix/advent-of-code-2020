fa6676cd42d2dc12984eda16d0e44e426978a065
IdeFFiX <ideffix1337@gmail.com>
2020-12-01 19:18:47 +0100

    Day 1 second task

--- src/main/kotlin/pl/ideffix/day1/Day1.kt
+++ src/main/kotlin/pl/ideffix/day1/Day1.kt
@@ -5,13 +5,27 @@ import kotlin.streams.toList
 
 fun main() {
     val nums = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day1/input.txt").stream().map { it.toInt() }.toList()
-    val pair = findSumOf(nums, 2020)
+    val pair = findSumOfTwo(nums, 2020)
     if (pair != null) {
-        print(pair.first * pair.second)
+        println(pair.first * pair.second)
+    }
+    val triple = findSumOfThree(nums, 2020)
+    if (triple != null) {
+        println(triple.first * triple.second * triple.third)
     }
 }
 
-fun findSumOf(nums: List<Int>, toFind: Int): Pair<Int, Int>? {
+fun findSumOfThree(nums: List<Int>, toFind: Int): Triple<Int, Int, Int>? {
+    for (num in nums) {
+        val pair = findSumOfTwo(nums, toFind - num)
+        if (pair != null) {
+            return Triple(pair.first, pair.second, num)
+        }
+    }
+    return null
+}
+
+fun findSumOfTwo(nums: List<Int>, toFind: Int): Pair<Int, Int>? {
     val complements = mutableListOf<Int>()
     for (num in nums) {
         if (num in complements) {
