20e2f9bc51144501f2446f9d3fc41bb05095afec
IdeFFiX <ideffix1337@gmail.com>
2020-12-04 13:49:14 +0100

    Day4 second task

--- src/main/kotlin/pl/ideffix/day4/Day4.kt
+++ src/main/kotlin/pl/ideffix/day4/Day4.kt
@@ -2,6 +2,8 @@ package pl.ideffix.day4
 
 import pl.ideffix.utils.FileUtils
 
+val validHeightTypes = listOf("cm", "in")
+
 fun main() {
     val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day4/input.txt")
     var pass = Password()
@@ -16,10 +18,10 @@ fun main() {
                 .forEach {
                     val split = it.split(':')
                     when (split[0]) {
-                        "byr" -> pass.byr = split[1]
-                        "iyr" -> pass.iyr = split[1]
-                        "eyr" -> pass.eyr = split[1]
-                        "hgt" -> pass.hgt = split[1]
+                        "byr" -> pass.byr = split[1].toInt()
+                        "iyr" -> pass.iyr = split[1].toInt()
+                        "eyr" -> pass.eyr = split[1].toInt()
+                        "hgt" -> pass.hgt = if (split[1].takeLast(2) in validHeightTypes) Height(split[1].take(split[1].length - 2).toInt(), split[1].takeLast(2)) else Height(-1, "bad")
                         "hcl" -> pass.hcl = split[1]
                         "ecl" -> pass.ecl = split[1]
                         "pid" -> pass.pid = split[1]
--- src/main/kotlin/pl/ideffix/day4/Password.kt
+++ src/main/kotlin/pl/ideffix/day4/Password.kt
@@ -1,22 +1,37 @@
 package pl.ideffix.day4
 
-class Password() {
-    var byr: String? = null
-    var iyr: String? = null
-    var eyr: String? = null
-    var hgt: String? = null
+val validColors = listOf(
+        "amb", "blu", "brn", "gry", "grn", "hzl", "oth"
+)
+
+class Password {
+    var byr: Int = -1
+    var iyr: Int = -1
+    var eyr: Int = -1
+    var hgt: Height? = null
     var hcl: String? = null
     var ecl: String? = null
     var pid: String? = null
     var cid: String? = null
     fun isValid(): Boolean =
-            !byr.isNullOrBlank() &&
-                    !iyr.isNullOrBlank() &&
-                    !eyr.isNullOrBlank() &&
-                    !hgt.isNullOrBlank() &&
-                    !hcl.isNullOrBlank() &&
-                    !ecl.isNullOrBlank() &&
-                    !pid.isNullOrBlank()
+            byr in 1920..2002 &&
+                    iyr in 2010..2020 &&
+                    eyr in 2020..2030 &&
+                    hgt != null && hgt!!.isValid() &&
+                    !hcl.isNullOrBlank() && hcl!!.matches(Regex("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\$")) &&
+                    ecl in validColors &&
+                    !pid.isNullOrBlank() && pid!!.matches(Regex("\\(|\\)|\\d{9}"))
 
 
+}
+
+class Height(val value: Int, val type: String) {
+    fun isValid(): Boolean {
+        if (type == "cm") {
+            return value in 150..193
+        } else if (type == "in") {
+            return value in 59..76
+        }
+        return false
+    }
 }
