8544e54305b8f20bbdb955b16d01df16e8edf89b
IdeFFiX <ideffix1337@gmail.com>
2020-12-07 15:58:09 +0100

    Day7 second task

--- src/main/kotlin/pl/ideffix/day7/Day7.kt
+++ src/main/kotlin/pl/ideffix/day7/Day7.kt
@@ -4,8 +4,8 @@ import pl.ideffix.utils.FileUtils
 
 fun main() {
     val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day7/input.txt")
-    val map = prepareStructure(lines)
-    print(countOccurrence("shiny gold", map, mutableSetOf()))
+    val map = prepareStructure2(lines)
+    print(countOccurrence2("shiny gold", map))
 }
 
 fun countOccurrence(bagName: String, map: Map<String, List<Pair<String, Int>>>, visited: MutableSet<String>): Int {
@@ -15,6 +15,15 @@ fun countOccurrence(bagName: String, map: Map<String, List<Pair<String, Int>>>, 
         if (visited.contains(b.first)) continue
         visited.add(b.first)
         count += countOccurrence(b.first, map, visited) + 1
+    }
+    return count
+}
+
+fun countOccurrence2(bagName: String, map: Map<String, List<Pair<String, Int>>>): Int {
+    var count = 0
+    val bag = map[bagName] ?: return count
+    for (b in bag) {
+        count += countOccurrence2(b.first, map) * b.second + b.second
     }
     return count
 }
@@ -36,6 +45,31 @@ private fun prepareStructure(
             val pair = name to howMany.toInt()
             if (mapEl == null) {
                 map[bagName] = mutableListOf(pair)
+            } else {
+                mapEl.add(pair)
+            }
+        }
+    }
+    return map
+}
+
+private fun prepareStructure2(
+    lines: List<String>
+): MutableMap<String, MutableList<Pair<String, Int>>> {
+    val map = mutableMapOf<String, MutableList<Pair<String, Int>>>()
+    for (line in lines) {
+        val split = line.split(' ')
+        val name = split.take(2).joinToString(" ")
+        val containedBags = split.subList(4, split.size).joinToString(" ").split(',')
+        for (bag in containedBags) {
+            val b = bag.trim().split(' ')
+            val howMany = b[0]
+            val bagName = b.subList(1, 3).joinToString(" ")
+            if (howMany == "no") continue
+            val mapEl = map[name]
+            val pair = bagName to howMany.toInt()
+            if (mapEl == null) {
+                map[name] = mutableListOf(pair)
             } else {
                 mapEl.add(pair)
             }
