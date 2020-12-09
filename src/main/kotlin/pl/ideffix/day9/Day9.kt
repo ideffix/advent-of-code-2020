4ebbd286a97d802b363a91e46f2be31947399f49
IdeFFiX <ideffix1337@gmail.com>
2020-12-09 18:40:44 +0100

    Day9 second task

--- src/main/kotlin/pl/ideffix/day9/Day9.kt
+++ src/main/kotlin/pl/ideffix/day9/Day9.kt
@@ -5,10 +5,24 @@ fun main() {
     val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day9/input.txt")
     val nums = lines.map{it.toLong()}.toList()
     val range = 25
+    var toFind = -1L
     for (i in range until nums.size) {
         val res = findSumOfTwo(nums.subList(i-range, i), nums[i])
         if (res == null) {
-            println(nums[i])
+            toFind = nums[i]
+        }
+    }
+    var sum = 0L
+    var arr = mutableListOf<Long>()
+    for (num in nums) {
+        arr.add(num)
+        sum += num
+        while (sum > toFind) {
+            sum -= arr.removeFirst()
+        }
+        if (sum == toFind) {
+            println(arr.min()!! + arr.max()!!)
+            break
         }
     }
 
