f0a3ca9d1d0430f1676e58f382e738d994ae7d55
IdeFFiX <ideffix1337@gmail.com>
2020-12-15 13:43:05 +0100

    Day15 second task

--- src/main/kotlin/pl/ideffix/day15/Day15.kt
+++ src/main/kotlin/pl/ideffix/day15/Day15.kt
@@ -4,25 +4,25 @@ import pl.ideffix.utils.FileUtils
 
 fun main() {
     val nums = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day15/input.txt")[0].split(',').map { it.toInt() }
-    println(firstTask(nums.toMutableList(), 2020))
+    println(firstTask(nums.toMutableList(), 30000000))
 }
 
 fun firstTask(nums: MutableList<Int>, end: Int): Int {
     val lastUsed = mutableMapOf<Int, MutableList<Int>>()
     for (i in nums.indices) {
-        val arr = lastUsed.getOrDefault(nums[i], mutableListOf())
+        val arr = lastUsed.getOrDefault(nums[i], ArrayDeque())
         arr.add(0, i)
         lastUsed[nums[i]] = arr
     }
     var i = nums.size
     while (i < end) {
         val prev = nums.last()
-        var arr = lastUsed.getOrDefault(prev, mutableListOf())
+        var arr = lastUsed.getOrDefault(prev, ArrayDeque())
         var current = 0
         if (arr.size >= 2) {
             current = arr[0] - arr[1]
         }
-        arr = lastUsed.getOrDefault(current, mutableListOf())
+        arr = lastUsed.getOrDefault(current, ArrayDeque())
         arr.add(0, i)
         nums.add(current)
         lastUsed[current] = arr
