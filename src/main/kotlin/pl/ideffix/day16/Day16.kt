5c3c36580daed4e25c7ba8659c78c9ac3e3822f0
IdeFFiX <ideffix1337@gmail.com>
2020-12-16 20:43:13 +0100

    Day16 second task

--- src/main/kotlin/pl/ideffix/day16/Day16.kt
+++ src/main/kotlin/pl/ideffix/day16/Day16.kt
@@ -2,16 +2,18 @@ package pl.ideffix.day16
 
 import pl.ideffix.utils.FileUtils
 
-data class Parsed(val rules: List<Pair<String, List<Pair<Int, Int>>>>, val yourTicket: List<Int>, val nearbyTickets: List<List<Int>>)
+data class Parsed(val rules: MutableList<Pair<String, List<Pair<Int, Int>>>>, val yourTicket: List<Int>, val nearbyTickets: MutableList<List<Int>>)
 
 fun main() {
     val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day16/input.txt")
     val parsed = parseInput(lines)
     println(solveFirst(parsed))
+    println(solveSecond(parsed))
 }
 
 fun solveFirst(parsed: Parsed): Int {
     var res = 0
+    val wrongTickets = mutableListOf<List<Int>>()
     for (ticket in parsed.nearbyTickets) {
         ticket@for (num in ticket) {
             for (ruleWrapper in parsed.rules) {
@@ -20,9 +22,33 @@ fun solveFirst(parsed: Parsed): Int {
                 }
             }
             res += num
+            wrongTickets.add(ticket)
         }
     }
+    parsed.nearbyTickets.removeAll(wrongTickets)
     return res
+}
+
+fun solveSecond(parsed: Parsed): Long {
+    val tickets = parsed.nearbyTickets.toMutableList()
+    tickets.add(parsed.yourTicket)
+    val res = mutableListOf<Pair<String, Int>>()
+    while(res.size < tickets[0].size) {
+        for (i in tickets[0].indices) {
+            val col = mutableListOf<Int>()
+            for (j in tickets.indices) {
+                col.add(tickets[j][i])
+            }
+            val a = parsed.rules.filter { rules ->
+                col.stream().allMatch{ num ->  rules.second.any { rule -> num >= rule.first && num <= rule.second }}
+            }
+            if (a.size == 1) {
+                res.add(a[0].first to i)
+                parsed.rules.removeIf { it == a[0] }
+            }
+        }
+    }
+    return res.filter { it.first.startsWith("departure") }.fold(1, { acc, pair ->  acc * parsed.yourTicket[pair.second]})
 }
 
 fun parseInput(lines: List<String>): Parsed {
