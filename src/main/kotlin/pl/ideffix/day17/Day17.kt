923cf5eb88f5089799db751254976be0e346d76d
IdeFFiX <ideffix1337@gmail.com>
2020-12-17 20:46:23 +0100

    Day17 first task

--- src/main/kotlin/pl/ideffix/day17/Day17.kt
+++ src/main/kotlin/pl/ideffix/day17/Day17.kt
@@ -2,46 +2,82 @@ package pl.ideffix.day17
 
 import pl.ideffix.utils.FileUtils
 
+const val cycles = 6
+
 fun main() {
-    val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day17/input.txt")
-    val matrix = listOf(createEmptyLayer(lines[0].length, lines.size), lines, createEmptyLayer(lines[0].length, lines.size))
+    val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day17/input.txt").map { it.toCharArray().toList() }
+    val matrix = createInitialMatrix(lines)
     println(solveFirst(matrix))
 }
 
-fun solveFirst(matrix: List<List<String>>): Int {
-    var currentMatrix = matrix.toMutableList()
-    val x = matrix[0][0].length
-    val y = matrix[0].size
-    for (cycle in 0 until 6) {
-        val newMatrix = mutableListOf<List<String>>()
-        if (!isEmptyLayer(currentMatrix.first())) {
-            currentMatrix.add(0, createEmptyLayer(x, y))
+fun createInitialMatrix(lines: List<List<Char>>): List<List<List<Char>>> {
+    val size = lines.size + (cycles+1) * 2
+    val matrix = createEmptyMatrix(size)
+    val mid  = size / 2
+    for (i in lines.indices) {
+        for (j in lines.indices) {
+            matrix[mid][i+ cycles+1][j+ cycles+1] = lines[i][j]
         }
-        if (!isEmptyLayer(currentMatrix.last())) {
-            currentMatrix.add(createEmptyLayer(x, y))
+    }
+    return matrix
+}
+
+fun createEmptyMatrix(size: Int): MutableList<MutableList<MutableList<Char>>> {
+    val matrix = mutableListOf<MutableList<MutableList<Char>>>()
+    for (i in 0 until size) {
+        val jList = mutableListOf<MutableList<Char>>()
+        for (j in 0 until size) {
+            val kList = mutableListOf<Char>()
+            for (k in 0 until size) {
+                kList.add('.')
+            }
+            jList.add(kList)
         }
-        for (l in matrix.indices) {
-            newMatrix.add(checkNewLayerState(matrix, l))
+        matrix.add(jList)
+    }
+    return matrix
+}
+
+fun solveFirst(matrix: List<List<List<Char>>>): Int {
+    var currentMatrix = matrix
+    val size = matrix.size
+    for (c in 0 until cycles) {
+        val newMatrix = createEmptyMatrix(matrix.size)
+        for (z in 1 until size - 1) {
+            for (y in 1 until size - 1) {
+                for (x in 1 until size - 1) {
+                    var active = 0
+                    for (i in -1..1) {
+                        for (j in -1..1) {
+                            for (k in -1..1) {
+                                if (i or j or k == 0) {
+                                    continue
+                                }
+                                if (currentMatrix[z+i][y+j][x+k] == '#') active++
+                            }
+                        }
+                    }
+                    when (currentMatrix[z][y][x]) {
+                        '#' -> {
+                            if (active == 2 || active == 3) {
+                                newMatrix[z][y][x] = '#'
+                            }
+                        }
+                        '.' -> {
+                            if (active == 3) {
+                                newMatrix[z][y][x] = '#'
+                            }
+                        }
+                    }
+                }
+            }
         }
         currentMatrix = newMatrix
     }
+
     return countActiveCubes(currentMatrix)
 }
 
-fun countActiveCubes(matrix: MutableList<List<String>>): Int =
-    matrix.flatten().fold(0, {acc, str -> acc + str.count { it == '#' }})
+fun countActiveCubes(matrix: List<List<List<Char>>>): Int =
+    matrix.flatten().flatten().count { it == '#' }
 
-
-fun checkNewLayerState(matrix: List<List<String>>, l: Int): List<String> {
-    TODO("Not yet implemented")
-}
-
-fun isEmptyLayer(layer: List<String>): Boolean = layer.all {line -> line.all { it == '.' }}
-
-fun createEmptyLayer(x: Int, y: Int): List<String> {
-    val l = mutableListOf<String>()
-    for (j in 0 until y) {
-        l.add(".".repeat(x))
-    }
-    return l
-}
