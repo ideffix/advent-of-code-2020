f57452045124d86900fae56384af93fbcd3f1471
IdeFFiX <ideffix1337@gmail.com>
2020-12-22 08:47:47 +0100

    Day22 second task

--- src/main/kotlin/pl/ideffix/day22/Day22.kt
+++ src/main/kotlin/pl/ideffix/day22/Day22.kt
@@ -5,32 +5,57 @@ import pl.ideffix.utils.FileUtils
 fun main() {
     val lines = FileUtils.readAllLines("src/main/kotlin/pl/ideffix/day22/input.txt")
     val pair = parseInput(lines)
-    println(solveFirst(pair))
+    //println(solveFirst(pair))
+    println(solveSecond(pair))
 }
 
-fun solveFirst(pair: Pair<ArrayDeque<Int>, ArrayDeque<Int>>): Int {
-    while (pair.first.isNotEmpty() && pair.second.isNotEmpty()) {
-        if (pair.first.first() > pair.second.first()) {
-            with(pair.first) {
-                addLast(first())
-                addLast(pair.second.first())
-            }
-        } else {
-            with(pair.second) {
-                addLast(first())
-                addLast(pair.first.first())
-            }
-        }
-        pair.first.removeFirst()
-        pair.second.removeFirst()
-    }
+fun solveSecond(pair: Pair<ArrayDeque<Int>, ArrayDeque<Int>>): Int {
+    val notEmpty = if (playSubGame(pair)) pair.first else pair.second
     var count = 0
-    val notEmpty = if (pair.first.isNotEmpty()) pair.first else pair.second
     for (i in 0 until notEmpty.size) {
         count += notEmpty[i] * (notEmpty.size - i)
     }
     return count
 }
+
+fun playSubGame(pair: Pair<ArrayDeque<Int>, ArrayDeque<Int>>): Boolean {
+    val states = mutableSetOf<Int>()
+    while (pair.first.isNotEmpty() && pair.second.isNotEmpty()) {
+        if (!states.add(pair.hashCode())) {
+            return true
+        }
+        val firstCard = pair.first.first()
+        val secondCard = pair.second.first()
+        pair.first.removeFirst()
+        pair.second.removeFirst()
+        val firstWon = if (pair.first.size >= firstCard && pair.second.size >= secondCard) {
+            playSubGame(ArrayDeque(pair.first.subList(0, firstCard)) to ArrayDeque(pair.second.subList(0, secondCard)))
+        } else {
+            firstCard > secondCard
+        }
+        if (firstWon) {
+            with(pair.first) {
+                addLast(firstCard)
+                addLast(secondCard)
+            }
+        } else {
+            with(pair.second) {
+                addLast(secondCard)
+                addLast(firstCard)
+            }
+        }
+    }
+    return pair.first.isNotEmpty()
+}
+
+//fun solveFirst(pair: Pair<ArrayDeque<Int>, ArrayDeque<Int>>): Int {
+//    var count = 0
+//    val notEmpty = if (playSubGame(pair)) pair.first else pair.second
+//    for (i in 0 until notEmpty.size) {
+//        count += notEmpty[i] * (notEmpty.size - i)
+//    }
+//    return count
+//}
 
 fun parseInput(lines: List<String>): Pair<ArrayDeque<Int>, ArrayDeque<Int>> {
     var firstDeck = ArrayDeque<Int>()
